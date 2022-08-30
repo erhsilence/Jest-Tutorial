943793b7289b56724ad2135f19990c6aac434009
汤景悦 <739580612@qq.com>
2022-08-30 16:16:22 +0800

    feat(components): title组件及测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import Title from 'components/Title';
 import React from 'react';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 
-const App = () => {
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <Button>点我</Button>
+//     </div>
+//   )
+// }
+
+const App = () =>{
   return (
     <div>
-      <h1>Hello</h1>
-      <Button>点我</Button>
+      <section>
+        <Title type='small' title='小字' />
+        <Title type='large' title='大字' />
+      </section>
     </div>
   )
 }
